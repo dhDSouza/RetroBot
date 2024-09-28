4dc154684e355d11d486a4f9a48ca41444f8345e
dhDSouza <dhs.danielsouza@gmail.com>
2024-09-28 17:12:49 -0300

    feat: add embeds to display recent achievements.

--- commands/achievements.py
+++ commands/achievements.py
@@ -1,3 +1,5 @@
+import discord
+
 from database.db import register_user, get_ra_username
 from utils.api import get_recent_achievements
 
@@ -10,10 +12,26 @@ async def fetch_user_achievements(message):
     if ra_username:
         achievements = get_recent_achievements(ra_username)
         if achievements:
-            response_message = f"**Últimas conquistas de {ra_username}:**\n\n"
             for ach in achievements:
-                response_message += f"🏆 **{ach['Title']}** em *{ach['GameTitle']}*\n"
-            await message.channel.send(response_message)
+                
+                hardcore_status = "Hardcore" if ach['HardcoreMode'] == 1 else "Softcore"
+                game_icon_url = f"https://retroachievements.org{ach['GameIcon']}"
+                achievement_icon_url = f"https://retroachievements.org{ach['BadgeURL']}"
+                
+                embed = discord.Embed(
+                    title = f"🏆 {ach['Title']}",
+                    description = f"*{ach['Description']}*",
+                    color = discord.Color.gold() if hardcore_status == "Hardcore" else discord.Color.dark_gray()
+                )
+                
+                embed.set_thumbnail(url=f"https://retroachievements.org{ach['GameIcon']}")
+                embed.set_image(url=f"https://retroachievements.org{ach['BadgeURL']}")
+                embed.add_field(name="Game", value=ach['GameTitle'], inline=True)
+                embed.add_field(name="Points", value=f"{ach['Points']} ({ach['TrueRatio']})", inline=False)
+                embed.add_field(name="Mode", value=hardcore_status, inline=True)
+                embed.add_field(name="Unloked in", value=ach['Date'], inline=False)
+                
+                await message.channel.send(embed=embed)
         else:
             await message.channel.send(f"Não consegui buscar as conquistas de `{ra_username}`.")
     else:
