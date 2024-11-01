3a15bc7b9009d90f1b2383e243778c8dfc789519
dhDSouza <dhs.danielsouza@gmail.com>
2024-11-01 11:56:50 -0300

    feat(database): Create challenge_players table to manage players participating in the challenge

--- database/db.py
+++ database/db.py
@@ -2,9 +2,11 @@ import sqlite3
 
 from datetime import datetime, timedelta
 
+
 def connect_db():
-    
+
     return sqlite3.connect('bot.db')
+
 
 def create_tables():
     with connect_db() as conn:
@@ -14,7 +16,7 @@ def create_tables():
                         discord_id TEXT PRIMARY KEY,
                         ra_username TEXT
                     )''')
-    
+
         c.execute('''CREATE TABLE IF NOT EXISTS challenges (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         console_name TEXT,
@@ -26,56 +28,72 @@ def create_tables():
                         end_date TEXT,
                         is_open INTEGER DEFAULT 1
                     )''')
-    
+
         c.execute('''CREATE TABLE IF NOT EXISTS achievements (
-                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     user_id TEXT,
                     achievement_id INTEGER,
-                    FOREIGN KEY (user_id) REFERENCES users(id)
+                    FOREIGN KEY (user_id) REFERENCES users(discord_id)
+                )''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS challenge_players (
+                    user_id TEXT, 
+                    challenge_id INTEGER,
+                    FOREIGN KEY (user_id) REFERENCES users(discord_id),
+                    FOREIGN KEY (challenge_id) REFERENCES challenges(id),
+                    PRIMARY KEY (user_id, challenge_id)
                 )''')
 
         conn.commit()
+
 
 def register_user(discord_id, ra_username):
     with connect_db() as conn:
         c = conn.cursor()
 
-        c.execute("INSERT OR REPLACE INTO users (discord_id, ra_username) VALUES (?, ?)", (discord_id, ra_username))
+        c.execute("INSERT OR REPLACE INTO users (discord_id, ra_username) VALUES (?, ?)",
+                  (discord_id, ra_username))
         conn.commit()
+
 
 def get_ra_username(discord_id):
     with connect_db() as conn:
         c = conn.cursor()
-        
-        c.execute("SELECT ra_username FROM users WHERE discord_id = ?", (discord_id,))
-        
+
+        c.execute(
+            "SELECT ra_username FROM users WHERE discord_id = ?", (discord_id,))
+
         result = c.fetchone()
 
         return result[0] if result else None
+
 
 def get_all_users():
     with connect_db() as conn:
         c = conn.cursor()
 
         c.execute("SELECT discord_id, ra_username FROM users")
-    
+
         users = c.fetchall()
-    
+
         return users
+
 
 def add_challenge(console_name, game_name, game_id, console_image_url, game_image_url):
     with connect_db() as conn:
         c = conn.cursor()
-    
+
         start_date = datetime.now()
         end_date = start_date + timedelta(days=7)
 
         c.execute('''INSERT INTO challenges (console_name, game_name, game_id, console_image_url, game_image_url, start_date, end_date, is_open)
                      VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
-                    (console_name, game_name, game_id, console_image_url, game_image_url, start_date, end_date, 1)
-                )
-    
+                  (console_name, game_name, game_id, console_image_url,
+                   game_image_url, start_date, end_date, 1)
+                  )
+
         conn.commit()
+
 
 def get_current_challenge():
     with connect_db() as conn:
@@ -85,7 +103,7 @@ def get_current_challenge():
                      WHERE start_date <= datetime('now') 
                      AND end_date >= datetime('now') 
                     ORDER BY id DESC LIMIT 1;''')
-    
+
         result = c.fetchone()
 
         if result:
@@ -102,7 +120,8 @@ def get_current_challenge():
             }
         else:
             return None
-    
+
+
 def check_challenge_status():
     with connect_db() as conn:
         c = conn.cursor()
@@ -111,7 +130,7 @@ def check_challenge_status():
                     WHERE start_date <= datetime('now') 
                     AND end_date <= datetime('now') 
                     ORDER BY id DESC LIMIT 1;''')
-    
+
         result = c.fetchone()
 
         if result:
@@ -129,60 +148,86 @@ def check_challenge_status():
         else:
             return None
 
+
 def finish_challenge(id):
     with connect_db() as conn:
         c = conn.cursor()
-    
+
         c.execute("UPDATE challenges SET is_open = 0 WHERE id = ?", (id,))
         conn.commit()
         rows_affected = c.rowcount
-        
-    
+
         return rows_affected > 0
+
 
 def update_challenge(new_challenge):
     with connect_db() as conn:
         c = conn.cursor()
-    
+
         current_challenge = get_current_challenge()
 
         if current_challenge:
             c.execute('''UPDATE challenges SET console_name = ?, game_name = ?, game_id = ?, console_image_url = ?, game_image_url = ?, start_date = ?, end_date = ?, is_open = ? WHERE id = ?''',
                       (new_challenge['console'], new_challenge['game'], new_challenge['game_id'], new_challenge['console_image_url'], new_challenge['game_image_url'], datetime.now(), datetime.now() + timedelta(days=7), 1, current_challenge['id']))
-        
+
             conn.commit()
-        
+
             if c.rowcount > 0:
-    
+
                 return True
             else:
-    
+
                 return False
         else:
             print("Não há um desafio ativo para ser atualizado.")
 
             return False
-        
+
+
 def save_achievement(user_id, achievement_id):
     with connect_db() as conn:
         c = conn.cursor()
 
-        c.execute("SELECT * FROM achievements WHERE user_id = ? AND achievement_id = ?", (user_id, achievement_id))
+        c.execute("SELECT * FROM achievements WHERE user_id = ? AND achievement_id = ?",
+                  (user_id, achievement_id))
         exists = c.fetchone()
 
         if exists is None:
-            c.execute('INSERT INTO achievements (user_id, achievement_id) VALUES (?, ?)', (user_id, achievement_id))
+            c.execute('INSERT INTO achievements (user_id, achievement_id) VALUES (?, ?)',
+                      (user_id, achievement_id))
             conn.commit()
         else:
-            print(f"A conquista {achievement_id} já está registrada para o usuário {user_id}.")
+            print(
+                f"A conquista {achievement_id} já está registrada para o usuário {user_id}.")
 
 
 def get_user_achievements(user_id):
     with connect_db() as conn:
         c = conn.cursor()
 
-        c.execute("SELECT achievement_id FROM achievements WHERE user_id = ?", (user_id,))
+        c.execute(
+            "SELECT achievement_id FROM achievements WHERE user_id = ?", (user_id,))
         rows = c.fetchall()
 
-
         return [row[0] for row in rows]
+
+
+def register_participation(discord_id, challenge_id):
+    with connect_db() as conn:
+        c = conn.cursor()
+
+        c.execute(
+            "INSERT INTO challenge_players (user_id, challenge_id) VALUES (?, ?)", (discord_id, challenge_id))
+        conn.commit()
+
+
+def get_participating_users(challenge_id):
+    with connect_db() as conn:
+        c = conn.cursor()
+
+        c.execute(
+            '''SELECT user_id FROM challenge_players WHERE challenge_id = ?''', (challenge_id,))
+
+        participants = c.fetchall()
+
+        return [row[0] for row in participants] if participants else []
